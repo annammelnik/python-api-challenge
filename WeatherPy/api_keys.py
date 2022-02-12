b66a2c0c98a6f4a57747caf8e4e3fab9584144f4
Anna Melnik <annammelnik@gmail.com>
2022-02-12 12:03:28 -0500

    final commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "58e24ac363e02400df27e267c2cdfd30"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCZpd3G1J6uq4JhfgkHr4G-dI8ww_k48l8"
